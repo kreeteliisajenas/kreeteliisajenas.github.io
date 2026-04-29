5e9f172cd74fd3ba577f6ff323795ce5d9af0012
kreeteliisajenas <kreeteliisa.jenas@gmail.com>
2026-04-29 12:52:39 +0300

    Add files via upload
    Initial website upload

---
+++ map1/js/javascript.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([58.378, 26.729], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
